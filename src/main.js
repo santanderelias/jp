a95a1448b13222bde255546c921e114ff86e7112
Elias Santander <cazagb@gmail.com>
2025-08-19 18:57:47 +0000

    small tweaks

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ console.log("main.js 0.1");
 import { japaneseCharacters, kanjiCharacters } from './database.js';
 //console.log(fnOne);
 //console.log(japaneseCharacters);
-document.getElementById('toolbar').addEventListener('click', () => {
-  console.log('navbar clicked');
+const homeSection = document.getElementById('home');
+const sectionOne = document.getElementById('sectionOne');
+homeSection.addEventListener('click', () => {
+  console.log('homeSection clicked');
+});
+sectionOne.addEventListener('click', () => {
+  console.log('sectionOne clicked');
 });
