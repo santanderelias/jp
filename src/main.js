f5b2afc8b901cb4313e7d521943f9c3df416f373
Elias Santander <cazagb@gmail.com>
2025-08-25 11:22:56 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 console.log("main.js 0.1");
+const menu = document.getElmentById('menu');
+const menuBtn = document.getElementById('menuBtn');
+menuBtn.addEventListener('click', () => { window.alert('hi'); });
 //import { japaneseCharacters, kanjiCharacters } from './database.js';
 //console.log(fnOne);
 //console.log(japaneseCharacters);
