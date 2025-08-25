ab3520f0edaf89f99b79453ffee5149564226f15
Elias Santander <cazagb@gmail.com>
2025-08-25 10:36:56 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 console.log("main.js 0.1");
-import { japaneseCharacters, kanjiCharacters } from './database.js';
+//import { japaneseCharacters, kanjiCharacters } from './database.js';
 //console.log(fnOne);
 //console.log(japaneseCharacters);
-const homeSection = document.getElementById('homeSection');
-const sectionOne = document.getElementById('sectionOne');
-homeSection.addEventListener('click', () => {
-  window.alert('いいえ');
-});
-sectionOne.addEventListener('click', () => {
-  window.alert('はい');
-});
+//const homeSection = document.getElementById('homeSection');
+//const sectionOne = document.getElementById('sectionOne');
+//homeSection.addEventListener('click', () => {
+//  window.alert('いいえ');
+//});
+//sectionOne.addEventListener('click', () => {
+//  window.alert('はい');
+//});
